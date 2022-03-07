0a57b31da9c724e91d0df043eb155b75e604bd29
kkam1336 <kskamtekar@gmail.com>
2022-03-06 21:42:52 -0700

    php test

--- form-handler.php
+++ form-handler.php
@@ -1,13 +1,13 @@
 <?php
-if(isset($_POST['submit'])){
-    $to = "compostingutah@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
+if(isset($_GET['submit'])){
+    $to = "comGETingutah@gmail.com"; // this is your Email address
+    $from = $_GET['email']; // this is the sender's Email address
+    $first_name = $_GET['first_name'];
+    $last_name = $_GET['last_name'];
     $subject = "Form submission";
     $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_GET['message'];
+    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_GET['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
